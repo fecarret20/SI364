30bb830e1bbc7f24e2888a85141efa103f1561cd
Fernando Carretero <fecarret@umich.edu>
2019-01-14 07:16:28 +0000

    another push

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mq=w2pr#z_)q_bb$6m1-eae=-w&^l+*c)^^8v$u!*6hd0j5k@s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
